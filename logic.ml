cd301504c0604845e024675b42dc17d24cda3273
Adam Dingle <adam@medovina.org>
2024-02-18 13:55:42 +0100

    Parse "let" and "suppose that"

--- logic.ml
+++ logic.ml
@@ -24,7 +24,13 @@ type term =
 let rec show_term = function
   | Const (id, _typ) -> id
   | Var (id, _typ) -> id
-  | App (t, u) -> sprintf "%s(%s)" (show_term t) (show_term u)
+  | App (t, u) -> (
+      match t, u with
+        | App (Const ("→", _), t), _ ->
+            sprintf "[%s → %s]" (show_term t) (show_term u)
+        | Const (q, _), Lambda (id, typ, u) when q == "∀" || q == "∃" ->
+            sprintf "%s%s:%s.%s" q id (show_type typ) (show_term u)
+        | _, _ -> sprintf "%s(%s)" (show_term t) (show_term u) )
   | Lambda (id, typ, t) -> sprintf "λ%s:%s.%s" id (show_type typ) (show_term t)
   | Eq (t, u) -> sprintf "%s = %s" (show_term t) (show_term u)
 
@@ -35,8 +41,10 @@ let not f = App (const "¬", f)
 let implies f g = App (App (const "→", f), g)
 
 let binder name id typ f = App (const name, Lambda (id, typ, f))
+let binder' name (id, typ) f = binder name id typ f
 
 let for_all = binder "∀"
+let for_all' = binder' "∀"
 
 let exists = binder "∃"
 
--- prover.ml
+++ prover.ml
@@ -4,11 +4,16 @@ open Logic
 
 let char_to_string = String.make 1
 
+let string_from s i = String.sub s i (String.length s - i)
+
 let (<<?) p q = attempt (p << q)
 
-let str s = spaces >>? attempt (string s)
+let str s =
+  let match_first c =
+    Char.lowercase_ascii c = Char.lowercase_ascii (s.[0]) in
+  spaces >>? satisfy match_first >>? string (string_from s 1)
 
-let id = spaces >>? (letter |>> char_to_string <|> string "ℕ")
+let id = spaces >>? (letter |>> char_to_string <|> string "𝔹" <|> string "ℕ")
 
 let sym = spaces >>? digit |>> char_to_string
 
@@ -20,6 +25,8 @@ let type_operators = [
 
 let typ = expression type_operators (id |>> fun id -> Base id)
 
+let id_typ = pair id (str ":" >> typ)
+
 let rec expression s = choice [
   (sym |>> fun c -> Const (c, unknown_type));
   (pipe2 (id <<? str "(") (expression << str ")")
@@ -27,22 +34,42 @@ let rec expression s = choice [
   id |>> fun v -> Var (v, unknown_type)
  ] s
 
-let eq_prop = pipe2 (expression <<? str "=") expression (fun f g -> Eq (f, g))
+let atomic = choice [
+  pipe2 (expression <<? str "=") expression (fun f g -> Eq (f, g));
+  expression << optional (str "is true")
+]
 
-let implies_prop = pipe2 (str "if" >> eq_prop) (str "then" >> eq_prop) implies
+let small_prop = pipe2 (choice [
+  pipe2 (atomic <<? str "implies") atomic implies;
+  atomic
+  ]) (option (str "for all" >> id_typ))
+  (fun p all_opt -> Option.fold all_opt ~none:p ~some:(fun id_typ -> for_all' id_typ p))
+
+let if_then_prop =
+  pipe2 (str "if" >> small_prop) (str "then" >> small_prop) implies
 
 let rec for_all_prop s = pipe3
-  (str "For all" >> sep_by1 id (str ","))
-  (str ":" >> typ) (str "," >> proposition)
+  (str "For all" >> sep_by1 id (str ",")) (str ":" >> typ) (str "," >> proposition)
   for_all_n s
 
-and not_exists_prop s = pipe3
-  (str "There is no" >> id) (str ":" >> typ) (str "such that" >> proposition)
-  (fun id typ p -> not (exists id typ p)) s
+and not_exists_prop s = pipe2
+  (str "There is no" >> id_typ) (str "such that" >> proposition)
+  (fun (id, typ) p -> not (exists id typ p)) s
 
-and proposition s = choice [for_all_prop; not_exists_prop; implies_prop; eq_prop] s
+and proposition s = choice [
+  for_all_prop; not_exists_prop; if_then_prop; small_prop
+] s
 
-let proposition_item = spaces >>? letter >>? string "." >> proposition << str "."
+let rec let_prop s = pipe2 (str "Let" >> id_typ << str ".") top_prop for_all' s
+
+and suppose s = pipe2
+  (str "Suppose that" >> sep_by1 proposition (str ", and that") << str ".")
+  (str "Then" >> proposition)
+  (List.fold_right implies) s
+
+and top_prop s = (let_prop <|> suppose <|> proposition) s
+
+let proposition_item = spaces >>? letter >>? string "." >> top_prop << str "."
 
 let propositions = many1 proposition_item
 
