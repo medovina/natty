a07c9a8c0aa6552c8981e230dcdbc31052babf6a
Adam Dingle <adam@medovina.org>
2024-05-22 17:04:06 +0200

    Reply to initialize request

--- server.ml
+++ server.ml
@@ -1,30 +1,44 @@
 open Unix
+open Yojson
+open Yojson.Basic.Util
 
 open Options
 open Printf
 open Util
 
+let read_message input =
+  let line = String.trim (input_line input) in
+  match opt_remove_prefix "Content-Length: " line with
+    | None ->
+        failwith "Content-Length expected"
+    | Some count ->
+        let line = String.trim (input_line input) in
+        if line <> "" then failwith "empty line expected"
+        else
+          let n = int_of_string count in
+          let bytes = Bytes.create n in
+          really_input input bytes 0 n;
+          Basic.from_string (String.of_bytes bytes)
+
 let run opts =
   if opts.pipe = "" then failwith "--pipe expected"
   else
-    printf "language server running: pipe = %s\n" opts.pipe;
-    let (input, _output) = open_connection (ADDR_UNIX opts.pipe) in
-    printf "connected\n";
+    printf "language server running: pipe = %s\n%!" opts.pipe;
+    let (input, output) = open_connection (ADDR_UNIX opts.pipe) in
+    printf "connected%!\n";
     
+    let init = read_message input in
+    let id = init |> member "id" in
+    assert (init |> member "method" |> to_string = "initialize");
+
+    let result = `Assoc [("capabilities", `Assoc [("textDocumentSync", `Int 1)])] in
+    let response = `Assoc [("jsonrpc", `String "2.0"); ("id", id); ("result", result)] in
+    let s = Basic.to_string response in
+    fprintf output "Content-Length: %d\r\n\r\n" (strlen s);
+    printf "sending %s\n%!" s;
+    fprintf output "%s%!" s;
+
     while (true) do
-      let line = String.trim (input_line input) in
-      match opt_remove_prefix "Content-Length: " line with
-        | None ->
-            print_endline line;
-            failwith "Content-Length expected"
-        | Some count ->
-            let line = String.trim (input_line input) in
-            if line <> "" then
-              failwith "empty line expected"
-            else
-              let n = int_of_string count in
-              let bytes = Bytes.create n in
-              really_input input bytes 0 n;
-              let s = String.of_bytes bytes in
-              print_endline s
+      let m = read_message input in
+      printf "%s\n%!" (Basic.to_string m)
     done
