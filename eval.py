b9f2c573bd41ed05ab630ed8ccb16f66c51555c7
Adam Dingle <adam@medovina.org>
2025-04-02 17:04:35 +0200

    Optionally record given clause counts in evaluations

--- eval.py
+++ eval.py
@@ -1,139 +1,83 @@
 import csv, multiprocessing, os, re, subprocess, sys, threading
 from collections import defaultdict
 from concurrent import futures
+from dataclasses import dataclass
 from os import path
 
 clear_line = '\033[K'
 
-timeout = default_timeout = 5
-timeout_suffix = ''
-eval_all = False
-eval_prover = None
-all_theorems = False
+default_timeout = 5
 
-i = 1
-while i < len(sys.argv):
-    arg = sys.argv[i]
-    if arg == '-a':
-        eval_all = True
-    elif arg == '-h':
-        all_theorems = True
-    elif arg.startswith('-p'):
-        eval_prover = arg[2:].lower()
-    elif arg.startswith('-t'):
-        timeout = int(arg[2:])
-        timeout_suffix = f'_{timeout}'
-    else:
-        break
-    i += 1
+@dataclass
+class Config:
+    dir: str
+    eval_provers: list
+    timeout: int
+    prove_steps: bool
+    stats: bool
 
-if i != len(sys.argv) - 1:
-    print(f'usage: {sys.argv[0]} [options...] <dir>')
-    print( '    -a: evaluate all provers')
-    print( '    -h: also try to prove theorems without proof steps')
-    print( '    -p<prover>: evaluate only the given prover')
-    print(f'    -t<num>: timeout (default is {default_timeout} seconds)')
-    sys.exit(1)
-dir = sys.argv[i]
+conf = Config('', [], default_timeout, True, False)
 
 all_provers = {
-    'Natty' : f'./natty -t{timeout}',
-    'E' : f'eprover-ho --auto -s --cpu-limit={timeout}',   # -s: silent
-    'Vampire' : f'vampire -t {timeout}',
-    'Zipperposition' : f'zipperposition --mode best --input tptp --timeout {timeout}'
+    'Natty' :
+        { 'cmd': './natty -t{timeout}', 'given': r'given clauses: (\d+)' },
+    'E' :   # -s: silent
+        { 'cmd': 'eprover-ho --auto -s --cpu-limit={timeout}',
+          'stats': '--print-statistics', 'given': r'# Processed clauses +: (\d+)' },
+    'Vampire' :
+        { 'cmd': 'vampire -t {timeout}' },
+    'Zipperposition' :
+        { 'cmd': 'zipperposition --mode best --input tptp --timeout {timeout}' }
 }
-
 all_prover_names = list(all_provers.keys())
-if eval_all:
-    eval_provers = all_prover_names
-elif eval_prover != None:
-    eval_provers = [p for p in all_prover_names if p.lower().startswith(eval_prover)]
-else:
-    eval_provers = [all_prover_names[0]]
 
-files = [name.removesuffix('.thf') for name in os.listdir(dir) if name.endswith('.thf')]
-files.sort(key = lambda s: [int(n) for n in s.replace('s', '').split('_')])
+def read_theorems():
+    files = [name.removesuffix('.thf') for name in os.listdir(conf.dir) if name.endswith('.thf')]
+    files.sort(key = lambda s: [int(n) for n in s.replace('s', '').split('_')])
 
-class Group:
-    def __init__(self, name):
-        self.results = {}
-        self.results_file = f'{dir}_{name}{timeout_suffix}.csv'
+    theorems = {}
+    for file in files:
+        with open(path.join(conf.dir, file + '.thf')) as f:
+            conjecture = f.readline().strip().removeprefix('% Problem: '.strip())
+        id = file.replace('_', '.')
+        if conf.prove_steps:
+            if '_s' in file:
+                include = True
+            else:
+                prefix = file.removesuffix('.thf') + '_s'
+                include = not any(f.startswith(prefix) for f in files)  # theorem has no steps
+        else:
+            include = '_s' not in file
+        if include:
+            theorems[id] = conjecture
+    return theorems
 
-    def read(self):
-        if path.exists(self.results_file):
-            with open(self.results_file) as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    name, conjecture = row[''], row['conjecture']
-                    row_ids = [k for k, v in self.results.items()
-                                 if (v[''], v['conjecture']) == (name, conjecture)]
-                    if row_ids != []:
-                        self.results[row_ids[0]] = row
+# Return a results dictionary which maps prover_name -> stat -> theorem_id -> value .
+def read_results(theorems, results_file):
+    results = defaultdict(lambda: defaultdict(dict))
 
-    def write(self):
-        proved : defaultdict = defaultdict(int)
-        total_time = defaultdict(float)
-        total_score = defaultdict(float)
-
-        for result in self.results.values():
-            for prover in all_prover_names:
-                r = result.get(prover)
-                if r != None and r != '':
-                    try:
-                        time = float(r)
-                        proved[prover] += 1
-                        total_time[prover] += time
-                        total_score[prover] += time
-                    except ValueError:
-                        total_score[prover] += 2 * timeout
-
-        provers = list(proved.keys())
-        with open(self.results_file, 'w') as out:
-            fieldnames = ['', 'conjecture'] + provers
-            writer = csv.DictWriter(out, fieldnames = fieldnames)
-            writer.writeheader()
-            writer.writerows(self.results.values())
-            out.write('\n')
-            n = len(self.results)
-
-            proved_row = {'' : f'proved (of {n})'}
-            for p in provers:
-                percent = proved[p] / n * 100
-                proved_row[p] = f'{proved[p]} ({percent:.0f}%)'
-            writer.writerow(proved_row)
-
-            avg_time = { prover : f'{t / proved[prover]:.2f}'
-                         for prover, t in total_time.items() }
-            avg_time[''] = 'average time'
-            writer.writerow(avg_time)
-
-            score = { prover : f'{t / n:.2f}' for prover, t in total_score.items() }
-            score[''] = 'PAR-2 score'
-            writer.writerow(score)
-
-            plural = '' if timeout == 1 else 's'
-            out.write(f'\nlimit = {timeout} second{plural}\n')
-
-thm_group = Group('theorems')
-step_group = Group('steps')
-groups = [thm_group, step_group] if all_theorems else [step_group]
-
-results = {}
-for file in files:
-    with open(path.join(dir, file + '.thf')) as f:
-        conjecture = f.readline().strip().removeprefix('% Problem: '.strip())
-    name = file.replace('_', '.')
-    data = { '' : f'thm {name}', 'conjecture' : conjecture }
-    if '_s' in file:
-        step_group.results[file] = data
-    else:
-        thm_group.results[file] = data
-        prefix = file.removesuffix('.thf') + '_s'
-        if not any(f.startswith(prefix) for f in files):  # theorem has no steps
-            step_group.results[file] = data
-
-for g in groups:
-    g.read()
+    if path.exists(results_file):
+        with open(results_file) as f:
+            reader = csv.reader(f)
+            for n, row in enumerate(reader):
+                if n == 0:
+                    header = row
+                    assert header[0:2] == ['', 'conjecture']
+                    continue
+                
+                if len(row) >= 2:
+                    name, conjecture = row[0:2]
+                    if name.startswith('thm '):
+                        id = name.removeprefix('thm ')
+                        if theorems.get(id) == conjecture:
+                            for col, val in zip(header[2:], row[2:]):
+                                if col[0].isupper():
+                                    prover = col
+                                    stat = 'time'
+                                else:
+                                    stat = col
+                                results[prover][stat][id] = val
+    return results
 
 proving = []
 lock = threading.Lock()
@@ -146,10 +90,18 @@ def prove(prover, file):
         proving.append(file)
         show_proving()
 
-    command = all_provers[prover]
-    filename = path.join(dir, file + '.thf')
+    prover_info = all_provers[prover]
+    cmd = prover_info['cmd']
+    cmd = cmd.replace('{timeout}', str(conf.timeout))
+    if conf.stats:
+        if stats := prover_info.get('stats'):
+            cmd += " " + stats
+        given_regex = prover_info.get('given')
+    else:
+        given_regex = None
 
-    cmd = command + " " + filename
+    filename = path.join(conf.dir, file + '.thf')
+    cmd += " " + filename
 
     completed = subprocess.run("time -f 'time:%U %S' " + cmd, shell = True, capture_output = True)
 
@@ -172,44 +124,168 @@ def prove(prover, file):
         else:
             status = 'Error'
 
+    match status:
+        case 'Theorem' | 'ContradictoryAxioms':  # contradictory axioms are still a proof
+            time = f'{elapsed:.2f}'
+        case 'GaveUp':
+            time = 'gave up'
+        case 'ResourceOut' | 'Timeout':
+            time = 'timeout'
+        case 'Error':
+            time = 'error'
+        case _:
+            print(f'unknown status: {text}')
+            assert False
+
+    res = {'time' : time }
+    if given_regex != None:
+        for line in lines:
+            if m := re.search(given_regex, line):
+                res['given'] = m[1]
+                break
+        else:
+            assert False, 'given clause count not found'
+
     with lock:
         print(clear_line + cmd)
         print(status)
         proving.remove(file)
         show_proving()
 
-    match status:
-        case 'Theorem' | 'ContradictoryAxioms':  # contradictory axioms are still a proof
-            res = f'{elapsed:.2f}'
-        case 'GaveUp':
-            res = 'gave up'
-        case 'ResourceOut' | 'Timeout':
-            res = 'timeout'
-        case 'Error':
-            res = 'error'
-        case _:
-            print(f'unknown status: {text}')
-            assert False
     return res
 
-def prove1(prover, file):
+def prove1(prover, id):
     try:
+        file = id.replace('.', '_')
         return prove(prover, file)
     except Exception as e:
         print(e)
         raise e
 
-for prover in eval_provers:
-    for group in groups:
-        files = []
-        for file, results in group.results.items():
-            r = results.get(prover)
-            if r == None or r == '':
-                files.append(file)
+def run_prover(theorems, prover, results):
+    ids = [id for id in theorems.keys() if id not in results[prover]['time']]
 
-        if files != []:
-            with futures.ThreadPoolExecutor(multiprocessing.cpu_count() // 2) as ex:
-                out = ex.map(lambda file: prove1(prover, file), files)
-            for file, res in zip(files, out):
-                group.results[file][prover] = res
-            group.write()
+    if ids != []:
+        with futures.ThreadPoolExecutor(multiprocessing.cpu_count() // 2) as ex:
+            out = ex.map(lambda id: prove1(prover, id), ids)
+        for id, stats in zip(ids, out):
+            for stat, val in stats.items():
+                results[prover][stat][id] = val
+        return True
+    else:
+        return False
+
+def write_results(theorems, results, results_file):
+    proved : defaultdict = defaultdict(int)
+    total_time = defaultdict(float)
+    total_score = defaultdict(float)
+
+    for prover, stats in results.items():
+        for id in theorems:
+            r = stats['time'].get(id)
+            if r != None and r != '':
+                try:
+                    time = float(r)
+                    proved[prover] += 1
+                    total_time[prover] += time
+                    total_score[prover] += time
+                except ValueError:
+                    total_score[prover] += 2 * conf.timeout
+
+    with open(results_file, 'w') as out:
+        prover_stats = [(prover, stats) for prover in all_prover_names
+                                        if (stats := results.get(prover))]
+        
+        writer = csv.writer(out)
+        header = ['', 'conjecture']
+        for prover, stats in prover_stats:
+            header.append(prover)
+            header += [stat for stat in stats if stat != 'time']
+        writer.writerow(header)
+
+        for id in theorems:
+            row = ['thm ' + id, theorems[id]]
+            for prover, stats in prover_stats:
+                for stat, vals in stats.items():
+                    row.append(vals.get(id, ''))
+            writer.writerow(row)
+
+        out.write('\n')
+        n = len(theorems)
+
+        proved_row = [f'proved (of {n})', '']
+        avg_row = ['average time', '']
+        par2_row = ['PAR-2 score', '']
+        stats_rows = [proved_row, avg_row, par2_row]
+
+        for prover, stats in prover_stats:
+            for stat in stats:
+                if stat == 'time':
+                    percent = proved[prover] / n * 100
+                    proved_row.append(f'{proved[prover]} ({percent:.0f}%)')
+
+                    avg_time = total_time[prover] / proved[prover]
+                    avg_row.append(f'{avg_time:.2f}')
+
+                    par2 = total_score[prover] / n
+                    par2_row.append(f'{par2:.2f}')
+                else:
+                    for row in stats_rows:
+                        row.append('')
+
+        for row in stats_rows:
+            writer.writerow(row)
+
+        plural = '' if conf.timeout == 1 else 's'
+        out.write(f'\nlimit = {conf.timeout} second{plural}\n')
+
+def parse_args():
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg == '-a':
+            conf.eval_provers = list(all_provers.keys())
+        elif arg == '-h':
+            conf.prove_steps = False
+        elif arg.startswith('-p'):
+            prefix = arg[2:].lower()
+            conf.eval_provers = [p for p in all_prover_names
+                                 if p.lower().startswith(prefix)]
+        elif arg == '-s':
+            conf.stats = True
+        elif arg.startswith('-t'):
+            conf.timeout = int(arg[2:])
+            # timeout_suffix = f'_{timeout}'
+        else:
+            break
+        i += 1
+
+    if i != len(sys.argv) - 1:
+        print(f'usage: {sys.argv[0]} [options...] <dir>')
+        print( '    -a: evaluate all provers')
+        print( '    -h: try to prove theorems without using proof steps')
+        print( '    -p<prover>: evaluate only the given prover')
+        print( '    -s: collect statistics')
+        print(f'    -t<num>: timeout (default is {default_timeout} seconds)')
+        exit(1)
+
+    if conf.eval_provers == []:
+        print('must specify prover(s) with -p or -a')
+        exit(1)
+
+    return sys.argv[i]
+
+def main():
+    conf.dir = parse_args()
+    theorems = read_theorems()
+
+    kind = 'steps' if conf.prove_steps else 'theorems'
+    timeout_suffix = '' if conf.timeout == default_timeout else f'_{conf.timeout}'
+    results_file = f'{conf.dir}_{kind}{timeout_suffix}.csv'
+    results = read_results(theorems, results_file)
+
+    for prover in conf.eval_provers:
+        if run_prover(theorems, prover, results):
+            write_results(theorems, results, results_file)
+
+main()
